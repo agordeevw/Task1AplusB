502b4bb878e5d28f93f66094775a4e94390f7f4a
agordeevw <agordeevw@gmail.com>
2018-09-17 00:40:59 +0700

    Added framework for task solution + kernel

--- src/main.cpp
+++ src/main.cpp
@@ -33,26 +33,77 @@ void reportError(cl_int err, const std::string &filename, int line)
 
 #define OCL_SAFE_CALL(expr) reportError(expr, __FILE__, __LINE__)
 
+template <class T>
+class OCLObject {
+public:
+  OCLObject() : object(nullptr) {}
+  ~OCLObject() { if (object) release<T>(); }
+  T& get() { return object; }
+
+private:
+  template <class U>
+  void release() { static_assert(false, "Invalid object type"); }
+
+  template<> void release<cl_context>() { clReleaseContext(object); }
+  template<> void release<cl_command_queue>() { clReleaseCommandQueue(object); }
+  template<> void release<cl_mem>() { clReleaseMemObject(object); }
+  template<> void release<cl_program>() { clReleaseProgram(object); }
+  template<> void release<cl_kernel>() { clReleaseKernel(object); }
+
+  T object;
+};
+
+class OCLTaskApp {
+public:
+  OCLTaskApp() {
+    if (!ocl_init())
+      throw std::runtime_error("Can't init OpenCL driver!");
+  }
+
+  void choose_device();
+  void create_context();
+  void create_command_queue();
+  void create_buffers_for_input(const std::vector<float>& as, const std::vector<float>& bs);
+  void create_buffer_for_output(std::vector<float>& cs);
+  void create_program(const std::string& kernel_source);
+  void try_compile_program();
+  void create_kernel();
+  void set_kernel_args(unsigned int size);
+  void execute_kernel(std::size_t work_group_size, std::size_t global_work_size);
+  void transfer_output_buffer_to_host(std::vector<float>& cs);
+
+private:
+  cl_device_id device = nullptr;
+  OCLObject<cl_context> context;
+  OCLObject<cl_command_queue> command_queue;
+  OCLObject<cl_mem> as_gpu;
+  OCLObject<cl_mem> bs_gpu;
+  OCLObject<cl_mem> cs_gpu;
+  OCLObject<cl_program> program;
+  OCLObject<cl_kernel> kernel;
+};
 
 int main()
-{    
-    // Пытаемся слинковаться с символами OpenCL API в runtime (через библиотеку clew)
-    if (!ocl_init())
-        throw std::runtime_error("Can't init OpenCL driver!");
+try
+{
+    OCLTaskApp app;
 
     // TODO 1 По аналогии с заданием Example0EnumDevices узнайте какие есть устройства, и выберите из них какое-нибудь
     // (если есть хоть одна видеокарта - выберите ее, если нету - выбирайте процессор)
+    app.choose_device();
 
     // TODO 2 Создайте контекст с выбранным устройством
     // См. документацию https://www.khronos.org/registry/OpenCL/sdk/1.2/docs/man/xhtml/ -> OpenCL Runtime -> Contexts -> clCreateContext
     // Не забывайте проверять все возвращаемые коды на успешность (обратите внимание что в данном случае метод возвращает
     // код по переданному аргументом errcode_ret указателю)
     // И хорошо бы сразу добавить в конце clReleaseContext (да, не очень RAII, но это лишь пример)
+    app.create_context();
 
     // TODO 3 Создайте очередь выполняемых команд в рамках выбранного контекста и устройства
     // См. документацию https://www.khronos.org/registry/OpenCL/sdk/1.2/docs/man/xhtml/ -> OpenCL Runtime -> Runtime APIs -> Command Queues -> clCreateCommandQueue
     // Убедитесь что в соответствии с документацией вы создали in-order очередь задач
     // И хорошо бы сразу добавить в конце clReleaseQueue
+    app.create_command_queue();
 
     unsigned int n = 1000*1000;
     // Создаем два массива псевдослучайных данных для сложения и массив для будущего хранения результата
@@ -72,6 +123,8 @@ int main()
     // Данные в as и bs можно прогрузить этим же методом скопировав данные из host_ptr=as.data() (и не забыв про битовый флаг на это указывающий)
     // или же через метод Buffer Objects -> clEnqueueWriteBuffer
     // И хорошо бы сразу добавить в конце clReleaseMemObject (аналогично все дальнейшие ресурсы вроде OpenCL под-программы, кернела и т.п. тоже нужно освобождать)
+    app.create_buffers_for_input(as, bs);
+    app.create_buffer_for_output(cs);
 
     // TODO 6 Выполните TODO 5 (реализуйте кернел в src/cl/aplusb.cl)
     // затем убедитесь что выходит загрузить его с диска (убедитесь что Working directory выставлена правильно - см. описание задания)
@@ -83,27 +136,34 @@ int main()
         if (kernel_sources.size() == 0) {
             throw std::runtime_error("Empty source file! May be you forgot to configure working directory properly?");
         }
-        // std::cout << kernel_sources << std::endl;
+        std::cout 
+          << "Kernel sources:\n"
+             "-------------------------\n" 
+          << kernel_sources 
+          << "-------------------------\n"
+          << std::endl;
     }
 
     // TODO 7 Создайте OpenCL-подпрограмму с исходниками кернела
     // см. Runtime APIs -> Program Objects -> clCreateProgramWithSource
     // у string есть метод c_str(), но обратите внимание что передать вам нужно указатель на указатель
+    app.create_program(kernel_sources);
     
     // TODO 8 Теперь скомпилируйте программу и напечатайте в консоль лог компиляции
     // см. clBuildProgram
-
     // А так же напечатайте лог компиляции (он будет очень полезен, если в кернеле есть синтаксические ошибки - т.е. когда clBuildProgram вернет CL_BUILD_PROGRAM_FAILURE)
     // см. clGetProgramBuildInfo
-//    size_t log_size = 0;
-//    std::vector<char> log(log_size, 0);
-//    if (log_size > 1) {
-//        std::cout << "Log:" << std::endl;
-//        std::cout << log.data() << std::endl;
-//    }
+    //    size_t log_size = 0;
+    //    std::vector<char> log(log_size, 0);
+    //    if (log_size > 1) {
+    //        std::cout << "Log:" << std::endl;
+    //        std::cout << log.data() << std::endl;
+    //    }
+    app.try_compile_program();
 
     // TODO 9 Создайте OpenCL-kernel в созданной подпрограмме (в одной подпрограмме может быть несколько кернелов, но в данном случае кернел один)
     // см. подходящую функцию в Runtime APIs -> Program Objects -> Kernel Objects
+    app.create_kernel();
 
     // TODO 10 Выставите все аргументы в кернеле через clSetKernelArg (as_gpu, bs_gpu, cs_gpu и число значений, убедитесь что тип количества элементов такой же в кернеле)
     {
@@ -113,8 +173,10 @@ int main()
         // clSetKernelArg(kernel, i++, ..., ...));
         // clSetKernelArg(kernel, i++, ..., ...));
     }
+    app.set_kernel_args(n);
 
     // TODO 11 Выше увеличьте n с 1000*1000 до 100*1000*1000 (чтобы дальнейшие замеры были ближе к реальности)
+    // (уже сделано)
     
     // TODO 12 Запустите выполнения кернела:
     // - С одномерной рабочей группой размера 128
@@ -130,6 +192,8 @@ int main()
         for (unsigned int i = 0; i < 20; ++i) {
             // clEnqueueNDRangeKernel...
             // clWaitForEvents...
+            app.execute_kernel(workGroupSize, global_work_size);
+
             t.nextLap(); // При вызове nextLap секундомер запоминает текущий замер (текущий круг) и начинает замерять время следующего круга
         }
         // Среднее время круга (вычисления кернела) на самом деле считаются не по всем замерам, а лишь с 20%-перцентайля по 80%-перцентайль (как и стандартное отклониение)
@@ -143,7 +207,10 @@ int main()
         // - Флопс - это число операций с плавающей точкой в секунду
         // - В гигафлопсе 10^9 флопсов
         // - Среднее время выполнения кернела равно t.lapAvg() секунд
-        std::cout << "GFlops: " << 0 << std::endl;
+        const std::size_t total_ops = n;
+        const double ops_per_second = static_cast<double>(n) / t.lapAvg();
+        const double gflops = ops_per_second / 1'000'000'000.0;
+        std::cout << "GFlops: " << gflops << std::endl;
 
         // TODO 14 Рассчитайте используемую пропускную способность обращений к видеопамяти (в гигабайтах в секунду)
         // - Всего элементов в массивах по n штук
@@ -151,7 +218,10 @@ int main()
         // - Обращений к видеопамяти т.о. 2*n*sizeof(float) байт на чтение и 1*n*sizeof(float) байт на запись, т.е. итого 3*n*sizeof(float) байт
         // - В гигабайте 1024*1024*1024 байт
         // - Среднее время выполнения кернела равно t.lapAvg() секунд
-        std::cout << "VRAM bandwidth: " << 0 << " GB/s" << std::endl;
+        const std::size_t total_vram_memory_accesses = 3 * n * sizeof(float);
+        const double vram_bandwidth = static_cast<double>(total_vram_memory_accesses) / t.lapAvg();
+        const double vram_bandwidth_in_gbytes = vram_bandwidth / static_cast<double>(1024 * 1024 * 1024);
+        std::cout << "VRAM bandwidth: " << vram_bandwidth_in_gbytes << " GB/s" << std::endl;
     }
 
     // TODO 15 Скачайте результаты вычислений из видеопамяти (VRAM) в оперативную память (RAM) - из cs_gpu в cs (и рассчитайте скорость трансфера данных в гигабайтах в секунду)
@@ -159,18 +229,28 @@ int main()
         timer t;
         for (unsigned int i = 0; i < 20; ++i) {
             // clEnqueueReadBuffer...
+            app.transfer_output_buffer_to_host(cs);
+            
             t.nextLap();
         }
         std::cout << "Result data transfer time: " << t.lapAvg() << "+-" << t.lapStd() << " s" << std::endl;
-        std::cout << "VRAM -> RAM bandwidth: " << 0 << " GB/s" << std::endl;
+
+        const std::size_t total_transfer_data_size = n * sizeof(float);
+        const double transfer_rate = static_cast<double>(total_transfer_data_size) / t.lapAvg();
+        const double transfer_rate_in_gbytes = transfer_rate / 1'000'000'000.0;
+        std::cout << "VRAM -> RAM bandwidth: " << transfer_rate_in_gbytes << " GB/s" << std::endl;
     }
 
     // TODO 16 Сверьте результаты вычислений со сложением чисел на процессоре (и убедитесь, что если в кернеле сделать намеренную ошибку, то эта проверка поймает ошибку)
-//    for (unsigned int i = 0; i < n; ++i) {
-//        if (cs[i] != as[i] + bs[i]) {
-//            throw std::runtime_error("CPU and GPU results differ!");
-//        }
-//    }
+    for (unsigned int i = 0; i < n; ++i) {
+      if (cs[i] != as[i] + bs[i]) {
+        throw std::runtime_error("CPU and GPU results differ!");
+      }
+    }
 
     return 0;
+} catch (const std::runtime_error& e) {
+  std::cout << "ERROR: " << e.what() << std::endl;
+} catch (const std::bad_alloc&) {
+  std::cout << "ERROR: not enough memory, close other applications and try again" << std::endl;
 }
